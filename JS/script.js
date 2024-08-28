572bebbe21dbf8e61c274017aa70d93add9216ac
Ifram Lopes <160543554+IfraimLopes@users.noreply.github.com>
2024-08-28 22:34:08 +0100

    update

---
+++ JS/script.js
@@ -0,0 +1,19 @@
+function click1() {
+  let imagemProduto = document.getElementById("product-image");
+  imagemProduto.src = "iphone_green.jpg"; // Corrija o nome do arquivo se necessário
+}
+
+function click2() {
+  let imagemProduto = document.getElementById("product-image");
+  imagemProduto.src = "iphone_silver.jpg";
+}
+
+function click3() {
+  let imagemProduto = document.getElementById("product-image");
+  imagemProduto.src = "iphone_golden.jpg";
+}
+
+function click4() {
+  let imagemProduto = document.getElementById("product-image");
+  imagemProduto.src = "iphone_blue.jpg"; // Corrigido para usar a variável correta
+}
